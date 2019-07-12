9b3f03e1e5669195c2dc6de3fcdb63da2ac410bd
chirs-paul <1142246268@qq.com>
2019-07-12 10:57:37 +0800

    首页搭建完成

--- src/router/index.js
+++ src/router/index.js
@@ -2,12 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 // @代表src文件夹
 import Login from '@/views/login'
+import Home from '@/views/home'
+
 // 注册vue
 Vue.use(VueRouter)
 const router = new VueRouter({
   // 配置路由规则
   routes: [
-    { name: 'login', path: '/login', component: Login }
+    { name: 'login', path: '/login', component: Login },
+    { name: 'home', path: '/', component: Home }
   ]
 })
 // 导出路由
